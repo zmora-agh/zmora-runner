947aec5c5eb522b4af3d362d12158a84b8989a79
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-31 20:38:07 +0200

    Json output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include "Runner.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+json toJson(const ProcessStats &stats) {
+  json j;
+  j["exit_code"] = stats.exit_code;
+  j["terminated_normally"] = stats.terminated_normally;
+  j["max_memory"] = stats.max_memory;
+  j["system_time"] = stats.system_time;
+  j["user_time"] = stats.user_time;
+  return j;
+}
 
 static std::vector<std::string> parsArgs(int argc, char **argv) {
   std::vector<std::string> args(argc - 1);
@@ -10,6 +23,7 @@ static std::vector<std::string> parsArgs(int argc, char **argv) {
 
 int main(int argc, char **argv) {
   Runner r;
-  r.Run(std::move(parsArgs(argc, argv)));
+  ProcessStats stats = r.Run(std::move(parsArgs(argc, argv)));
+  std::cerr << toJson(stats) << std::flush;
   return 0;
 }
